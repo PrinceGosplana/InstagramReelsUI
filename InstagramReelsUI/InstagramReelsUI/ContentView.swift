bcf5eeeb7cf6143e4e887627b9453967d399cdeb
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-06 11:38:32 +0300

    init project

---
+++ InstagramReelsUI/InstagramReelsUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstagramReelsUI
+//
+//  Created by Oleksandr Isaiev on 06.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstagramReelsUI/InstagramReelsUI/InstagramReelsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramReelsUIApp.swift
+//  InstagramReelsUI
+//
+//  Created by Oleksandr Isaiev on 06.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramReelsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
