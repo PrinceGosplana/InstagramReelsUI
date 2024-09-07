a93a7c067f07525f7eec45443fe79da6a3db78cf
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-07 10:53:28 +0300

    ReelsPlayer appearance

--- InstagramReelsUI/InstagramReelsUI/View/ReelsPlayer.swift
+++ InstagramReelsUI/InstagramReelsUI/View/ReelsPlayer.swift
@@ -10,12 +10,79 @@ import SwiftUI
 struct ReelsPlayer: View {
 
     @Binding var reel: Reel
+    /// Expanding title if its clicked
+    @State private var showMore = false
+    private let sampleText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
 
     var body: some View {
         ZStack {
             if let player = reel.player {
                 CustomVideoPlayer(player: player)
+
+                VStack {
+                    HStack(alignment: .bottom) {
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack(spacing: 15) {
+                                Image(.profilePicture)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 30, height: 30)
+                                    .clipShape(Circle())
+
+                                Text("Juventus")
+                                    .font(.callout.bold())
+
+                                Button {
+                                    
+                                } label: {
+                                    Text("Follow")
+                                        .font(.caption.bold())
+                                }
+                            }
+
+                            /// title custom view
+                            ZStack {
+                                if showMore {
+                                    ScrollView(.vertical, showsIndicators: false) {
+                                        /// extra text
+                                        Text(reel.mediaFile.title + sampleText)
+                                            .font(.callout)
+                                            .fontWeight(.semibold)
+                                    }
+                                    .frame(height: 120)
+                                } else {
+                                    Button {
+                                        withAnimation {
+                                            showMore.toggle()
+                                        }
+                                    } label: {
+                                        HStack {
+                                            Text(reel.mediaFile.title)
+                                                .font(.callout)
+                                                .fontWeight(.semibold)
+                                                .lineLimit(1)
+
+                                            Text("more")
+                                                .font(.callout.bold())
+                                                .foregroundStyle(.gray)
+                                        }
+                                        .padding(.top, 6)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                    }
+                                }
+                            }
+                        }
+
+                        Spacer(minLength: 20)
+                    }
+                }
+                .foregroundStyle(.white)
+                .frame(maxHeight: .infinity, alignment: .bottom)
             }
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
